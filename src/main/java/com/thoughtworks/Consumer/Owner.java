f8d613bd084d91d3c1a1b2b4f158ea981b0c4123
snehamanojraopalekar <nehapalekar026@gmail.com>
2019-10-05 11:45:44 +0530

    Add. class Owner

---
+++ src/main/java/com/thoughtworks/Consumer/Owner.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.Consumer;
+
+public class Owner implements IOwner{
+
+    @Override
+    public void informFullSpace() {
+
+    }
+
+    @Override
+    public void informFreeSpace() {
+
+    }
+}
