70589afe08454a2ad474248852e6e369c1a9348c
snehamanojraopalekar <nehapalekar026@gmail.com>
2019-10-05 11:49:09 +0530

    Add. class SecurityGuard

---
+++ src/main/java/com/thoughtworks/Consumer/SecurityGuard.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.Consumer;
+
+public class SecurityGuard implements IOwner{
+    @Override
+    public void informFullSpace() {
+
+    }
+
+    @Override
+    public void informFreeSpace() {
+
+    }
+
+}
